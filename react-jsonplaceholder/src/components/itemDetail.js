95532fcbc06456819f5bca095c5c5781024b91e3
DolapoSoares <soaresdolapo@gmail.com>
2024-06-15 14:24:51 +0100

    creating the itemDetail.js file in the component folder

--- react-jsonplaceholder/src/components/itemDetail.js
+++ react-jsonplaceholder/src/components/itemDetail.js
@@ -1,11 +1,44 @@
 import React , { useEffect, useState} from "react";
 import axios from 'axios';
 import { useParams, useHistory} from `react-router-dom`
+import { Link } from "react-router-dom";
 
-const EditItem = () => {
+const ItemDetail = () => {
+
+    const { id } = useParams();
+    const[item, setItem] = useState(null);
+    const history = useHistory();
+   
+    useEffect(() => {
+        fetchItems();
+    },[id]);
+
+    const fetchItems = async () => {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/guide/`);
+        setItem(res.data)
+    }
+
+    const handleDelete = async () => {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/guide/`);
+        history.push('/');
+    }
     return(
         <div>
-            
+            {item ? (
+                <div>
+                    <h1>{item.title}</h1>
+                    <p>{item.body}</p>
+                    <Link  >
+                        <button>Edit</button>
+                    </Link>
+                    <button onClick={handleDelete}>Delete</button>
+                </div>
+                ) : (
+                    <p>Loading...</p>
+                    )}
+                   
         </div>
     )
 }
+
+export default ItemDetail;
