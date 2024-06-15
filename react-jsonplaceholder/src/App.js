f91f1795ef510e118628f2c60b4958a1b0ca1524
DolapoSoares <soaresdolapo@gmail.com>
2024-06-15 16:16:18 +0100

    Replaced Switch with Routes

--- react-jsonplaceholder/src/App.js
+++ react-jsonplaceholder/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ItemList from './components/ItemList';
-import ItemDetail from './components/ItemDetail';
-import EditItem from './components/EditItem';
+import ItemDetail from './components/itemDetail';
+import EditItem from './components/editDetails';
 
 function App() {
   return (
     <Router>
       <div className="Routes">
-        <Switch>
+        <Routes>
           <Route exact path="/" component={ItemList} />
           <Route path="/item/:id" component={ItemDetail} />
           <Route path="/edit/:id" component={EditItem}/>
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
---
+++ react-jsonplaceholder/src/components/editDetails.js
@@ -0,0 +1,50 @@
+import React , { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, useHistory } from 'react-router-dom';
+
+const EditItem = () => {
+    const { id } = useParams();
+    const [item, setItem] = useState({ title :"", body: ""});
+    const history = useHistory();
+
+    useEffect(() => {
+        fetchItem();
+
+    },[id]);
+
+
+  const fetchItem = async () => {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/guide/`);
+    setItem(res.data);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setItem({ ...item, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.put(`https://jsonplaceholder.typicode.com/guide/`, item);
+    history.push(`/item/${id}`);
+  };
+
+  return (
+    <div>
+      <h1>Edit Item</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title</label>
+          <input type="text" name="title" value={item.title} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Body</label>
+          <textarea name="body" value={item.body} onChange={handleChange}></textarea>
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditItem;
