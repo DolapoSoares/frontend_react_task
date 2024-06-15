7387a15fea41fd2116053805d25acc158d9761d7
DolapoSoares <soaresdolapo@gmail.com>
2024-06-15 10:23:41 +0100

    creating the editItems.js file in the component folder

--- react-jsonplaceholder/src/components/editItems.js
+++ react-jsonplaceholder/src/components/editItems.js
@@ -1,3 +1,9 @@
 import React , { useEffect, useState} from "react";
 import axios from 'axios';
 import { useParams, useHistory} from `react-router-dom`
+
+const EditItem = () => {
+    return(
+        
+    )
+}
