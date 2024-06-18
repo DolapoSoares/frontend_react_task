e7f6d4daf1dc42396019397a77b0b782fb471512
DolapoSoares <soaresdolapo@gmail.com>
2024-06-18 20:12:16 +0100

    fix: build

--- react-jsonplaceholder/src/App.js
+++ react-jsonplaceholder/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import ItemList from './components/ItemList';
-import ItemDetail from './components/ItemDetail';
+import ItemDetail from './components/itemDetail';
 import EditItem from './components/editDetails';
 
 function App() {
